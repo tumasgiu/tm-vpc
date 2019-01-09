7293641179c2a76cc1b99a8cf5d64e64976510e5
Thomas Franceschini <thomas.franceschini@groundhogsec.com>
2019-01-09 14:14:17 +0100

    fix deprecation warning

--- internal_zone.tf
+++ internal_zone.tf
@@ -1,7 +1,9 @@
-
 resource "aws_route53_zone" "internal" {
 	count = "${var.internal_domain == "" ? 0 : 1}"
 
-	vpc_id = "${aws_vpc.main.id}"
-	name = "${var.internal_domain}"
+	vpc {
+		vpc_id = "${aws_vpc.main.id}"
+	}
+
+	name  = "${var.internal_domain}"
 }
